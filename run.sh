18ed7b537d3140c18c5e03550a46108deab4f519
yartFF <carbonel.q@gmail.com>
2014-11-27 00:46:20 +0100

    + args handlers

--- run.sh
+++ run.sh
@@ -1,26 +1,51 @@
 #!/bin/sh
 
-count=1
-command=""
+readonly CMD_PUSH='edit'
+readonly CMD_PULL='update'
+readonly DIR_FILE='files/'
 
-while [ $count -le $# ]
-do
-  if [ $count -eq 1 ]
-  then
-    command="$1" 
-  else
-    args[$((count - 1))]="${!count}"
-  fi
-  count=$((count + 1))
-done
+usage() {
+  echo -n Usage:' '
+  echo sh $0 "{$CMD_PUSH,$CMD_PULL}" [FILE]...;
+  exit
+}
 
-count=1
-echo command: $command
+if [ $# -eq 0 ]; then
+  usage
+elif [ $# -le 1 ]; then
+  count=0;
+  for f in "$DIR_FILE"/.*
+  do
+    f=`echo $f | grep -o '[^/]*$'`
+    if [ $f != "." ] && [ $f != ".." ]; then
+      args[$count]="$f"
+      count=$((count + 1))
+    fi
+  done
+else
+  count=2
+  while [ $count -le $# ]
+  do
+    args[$((count - 2))]=`echo "${!count}" | grep -o '[^/]*$'`
+    count=$((count + 1))
+  done
+fi
+
+if [ $1 == $CMD_PUSH ]; then
+  dir_from="~/"
+  dir_to="$DIR_FILE"
+elif [ $1 == $CMD_PULL ]; then
+  dir_from="$DIR_FILE/"
+  dir_to="~/"
+else
+  usage
+fi
+
+echo $1
+echo "$dir_from -> $dir_to"
+count=0
 while [ ${args[$count]} ]
 do
-  echo -n arg: 
   echo ${args[$count]}
   count=$((count + 1))
 done
-
-echo $count
