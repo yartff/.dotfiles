dad126015b605acd9bda603a9cdc653e8611a5c3
yartFF <carbonel.q@gmail.com>
2014-11-29 16:38:42 +0100

    + added diff command

--- run.sh
+++ run.sh
@@ -2,13 +2,24 @@
 
 readonly CMD_PUSH='modify'
 readonly CMD_PULL='install'
+readonly CMD_DIFF='diff'
 readonly DIR_HOME="$HOME"
 readonly DIR_FILE="$DIR_HOME/.dotfiles/files"
 
 usage() {
   echo -n Usage:' '
-  echo sh $0 "{$CMD_PUSH,$CMD_PULL}" [FILE]...;
+  echo sh $0 "{$CMD_PUSH,$CMD_PULL,$CMD_DIFF}" [FILE]...;
   exit
+}
+
+diff_dotfiles() {
+  count_=0
+  echo "Diff:"
+  while [ "${args[$count_]}" ]
+  do
+    diff "$1/${args[$count_]}" "$2/${args[$count_]}" -qr
+    count_=$((count_ + 1))
+  done
 }
 
 sync_dotfiles() {
@@ -31,7 +42,6 @@ elif [ $# -le 1 ]; then
   all_files="$DIR_FILE/.*"
   count=0;
 
-  echo $all_files
   for tmp in $all_files
   do
     tmp=`echo $tmp | grep -o '[^/]*$'`
@@ -53,6 +63,8 @@ if [ $1 == $CMD_PUSH ]; then
   sync_dotfiles "$DIR_HOME" "$DIR_FILE" $1;
 elif [ $1 == $CMD_PULL ]; then
   sync_dotfiles "$DIR_FILE" "$DIR_HOME" $1;
+elif [ $1 == $CMD_DIFF ]; then
+  diff_dotfiles "$DIR_HOME" "$DIR_FILE"
 else
   usage;
 fi
