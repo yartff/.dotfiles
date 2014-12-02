fd64d65b93ac060a8ed40b8c92acb6bca2b6c541
yartFF <carbonel.q@gmail.com>
2014-12-02 03:31:35 +0100

    + Rewrite, push OP

--- run.sh
+++ run.sh
@@ -3,12 +3,15 @@
 readonly CMD_PUSH='push'
 readonly CMD_PULL='pull'
 readonly CMD_DIFF='diff'
+readonly CMD_CHECK='checkout'
 readonly DIR_HOME="$HOME"
 readonly DIR_FILE="$DIR_HOME/.dotfiles/files"
+readonly CP_COMM="cp -uvr "
+shopt -s dotglob
 
 usage() {
   echo -n Usage:' '
-  echo sh $0 "{$CMD_PUSH,$CMD_PULL,$CMD_DIFF}" [FILE]...;
+  echo sh $0 "{$CMD_PUSH,$CMD_PULL,$CMD_DIFF,$CHECKOUT}" [FILE]...;
   exit
 }
 
@@ -25,31 +28,33 @@ diff_dotfiles() {
   done
 }
 
-sync_dotfiles() {
-  echo "$3ing..." ## lel
+sync_dotfiles_push() {
+  echo "Pushing..."
   count_sync=0
   while [ "${args[$count_sync]}" ]
   do
     obj="${args[$count_sync]}"
-    ls $2/$obj > /dev/null 2>&1
-    if [ $? -eq 1 ]; then
-      cp -vr "$1/$obj" "$2" 2> /dev/null;
+    if [ -d "$DIR_HOME/$obj" -a -f "$DIR_FILE/$obj" -o \
+      -f "$DIR_HOME/$obj" -a -d "$DIR_FILE/$obj" ]; then
+      rm -rfv "$DIR_FILE/$obj";
+    fi
+    if [ ! -f "$DIR_HOME/$obj" -a ! -d "$DIR_HOME/$obj" ]; then
+      rm -rfv "$DIR_FILE/$obj"
+      return
+    fi
+    mkdir -vp "$DIR_FILE/`dirname $obj`"
+    if [ -d "$DIR_HOME/$obj" -a -d "$DIR_FILE/$obj" ]; then
+      $CP_COMM $DIR_HOME/$obj/* "$DIR_FILE/$obj"
     else
-      if [ -f "$1/$obj" -a ! -f "$2/$obj" -o -d "$1/$obj" -a ! -d "$2/$obj" ];
-      then
-	rm -rfv $2/$obj;
-	mkdir -vp "$2/`dirname $obj`"
-	cp -vr $1/$obj $2/$obj #2> /dev/null;
-      else
-	diff "$1/$obj" "$2/$obj" -q > /dev/null
-	if [ $? -eq 1 ]; then
-	  cp -vr "$1/$obj" "$2" 2> /dev/null
-	fi
-      fi
+      $CP_COMM "$DIR_HOME/$obj" "$DIR_FILE/$obj"
     fi
     count_sync=$((count_sync + 1))
   done
-  echo "Done."
+}
+
+re_checkout() {
+  rm -rf "$DIR_FILE" 2> /dev/null
+  git checkout "$DIR_FILE"
 }
 
 if [ $# -eq 0 ]; then
@@ -77,11 +82,15 @@ else
 fi
 
 if [ $1 == $CMD_PUSH ]; then
-  sync_dotfiles "$DIR_HOME" "$DIR_FILE" $1;
+  sync_dotfiles_push "$DIR_HOME" "$DIR_FILE"
 elif [ $1 == $CMD_PULL ]; then
-  sync_dotfiles "$DIR_FILE" "$DIR_HOME" $1;
+  echo Pull Not ok
+  ## sync_dotfiles "$DIR_FILE" "$DIR_HOME" $1;
 elif [ $1 == $CMD_DIFF ]; then
   diff_dotfiles "$DIR_HOME" "$DIR_FILE"
+elif [ $1 == $CMD_CHECK ]; then
+  re_checkout
 else
   usage;
 fi
+echo "Done."
