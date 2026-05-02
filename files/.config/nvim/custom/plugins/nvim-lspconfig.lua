fde876ba38b4e10e0ad887bd7c6c26a1aed6a067
yartFF <2074648+yartff@users.noreply.github.com>
2026-05-02 18:05:32 +0200

    = sync

--- files/.config/nvim/custom/plugins/nvim-lspconfig.lua
+++ files/.config/nvim/custom/plugins/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     })
 
     --[[ Diagnostic ]]
-    vim.keymap.set('n', '<leader>e',  vim.diagnostic.open_float,    opts)
+    vim.keymap.set('n', '<leader>d',  vim.diagnostic.open_float,    opts)
     vim.keymap.set('n', ']d',         vim.diagnostic.goto_next,     opts)
     vim.keymap.set('n', '[d',         vim.diagnostic.goto_prev,     opts)
     vim.keymap.set('n', '<leader>q',  vim.diagnostic.setloclist,    opts)
